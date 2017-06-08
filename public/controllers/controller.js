c829db92a404c5adcd5dc2381e4e6dd55f305c28
Nguyen Dao <ndao1900@gmail.com>
2017-06-08 15:05:13 -0700

    Installed mongojs and body-parser. Can now add contacts to DB.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,8 +3,24 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http',
   function($scope,$http){
     console.log("Hello World from controller");
-    $http.get('/contactlist').success(function(res){
-      console.log("controller got the data");
-      $scope.contactlist = res;
-    });
+    var refresh = function(){
+      $http.get('/contactlist').success(function(res){
+        console.log("controller got the data");
+        $scope.contactlist = res;
+        $scope.contact = "";
+      });
+    };
+
+    refresh();
+
+    $scope.addContact = function() {
+      console.log($scope.contact);
+      $http.post('/contactlist', $scope.contact).success(
+        function(res){
+          console.log("Successfully added the contact!");
+          console.log(res);
+          refresh();
+        }
+      );
+    };
   }]);
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 function Person(name, email, number) {
     this.name = name;
@@ -8,12 +11,22 @@ function Person(name, email, number) {
 }
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 app.get('/contactlist',function(req,res){
   console.log("Server received a GET request");
-  var person1 = new Person('Tim', 'tim@email.com', '(111)111-1111');
-  var person2 = new Person('Emily', 'em@email.com', '(222)111-1111');
-  var contactlist = [person1, person2];
-  res.json(contactlist);
+
+  db.contactlist.find(function(err,docs){
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/contactlist', function(req,res){
+  console.log("getting contact from the request");
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err,doc){
+    res.json(doc);
+  })
 });
 app.listen(3000);
 console.log("Server running on port 3000");
