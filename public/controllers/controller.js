f04aa031fb2c5c4a4bd5a3d458587fed426118af
Nguyen Dao <ndao1900@gmail.com>
2017-06-08 14:01:50 -0700

    Added nodemon, retrieving dummy info from server and display it in index.thml

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp',[]);
+
 myApp.controller('AppCtrl',['$scope','$http',
   function($scope,$http){
     console.log("Hello World from controller");
+    $http.get('/contactlist').success(function(res){
+      console.log("controller got the data");
+      $scope.contactlist = res;
+    });
   }]);
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express')
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+function Person(name, email, number) {
+    this.name = name;
+    this.email = email;
+    this.number = number;
+}
 
+app.use(express.static(__dirname + "/public"));
+app.get('/contactlist',function(req,res){
+  console.log("Server received a GET request");
+  var person1 = new Person('Tim', 'tim@email.com', '(111)111-1111');
+  var person2 = new Person('Emily', 'em@email.com', '(222)111-1111');
+  var contactlist = [person1, person2];
+  res.json(contactlist);
+});
 app.listen(3000);
 console.log("Server running on port 3000");
