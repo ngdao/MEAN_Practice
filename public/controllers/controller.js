fa556df07cbcd0a2c610318b02e1ebd8a85388f1
Nguyen Dao <ndao1900@gmail.com>
2017-06-08 19:25:53 -0700

    Finish all the functionalities for the tutorial!

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,10 +2,8 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl',['$scope','$http',
   function($scope,$http){
-    console.log("Hello World from controller");
     var refresh = function(){
       $http.get('/contactlist').success(function(res){
-        console.log("controller got the data");
         $scope.contactlist = res;
         $scope.contact = "";
       });
@@ -14,18 +12,33 @@ myApp.controller('AppCtrl',['$scope','$http',
     refresh();
 
     $scope.addContact = function() {
-      console.log($scope.contact);
-      $http.post('/contactlist', $scope.contact).success(
-        function(res){
-          console.log("Successfully added the contact!");
-          console.log(res);
-          refresh();
-        }
-      );
+
+      if (typeof($scope.contact._id) == "undefined")
+      {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(
+          function(res){
+            console.log("Successfully added the contact!");
+            refresh();
+          }
+        );
+      }
     };
 
     $scope.remove = function(id){
       $http.delete('/contactlist/'+ id).success(function(res){
+        refresh();
+      });
+    };
+
+    $scope.edit= function(id){
+      $http.get('/contactlist/'+id).success(function(res){
+        $scope.contact = res;
+      });
+    };
+
+    $scope.update = function(){
+      $http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(res){
         refresh();
       });
     };
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/contactlist',function(req,res){
 app.post('/contactlist', function(req,res){
   console.log("getting contact from the request");
   console.log(req.body);
-  db.contactlist.insert(req.body, function(err,doc){
+    db.contactlist.insert({name: req.body.name, email: req.body.email, number: req.body.number}, function(err,doc){
     res.json(doc);
   })
 });
@@ -35,5 +35,23 @@ app.delete('/contactlist/:id', function(req,res){
     res.json(doc);
   });
 });
+
+app.get('/contactlist/:id', function(req,res){
+  var id = req.params.id;
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err,doc){
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function(req,res){
+  var id =  req.params.id;
+  db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email,number: req.body.number}},
+    new:true},function(err,doc){
+      res.json(doc);
+    }
+  );
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
